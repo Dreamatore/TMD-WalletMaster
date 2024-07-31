4638f2c9523d45669150c452a0fdcbd1e5bf8483
Dreamatore <129581708+Dreamatore@users.noreply.github.com>
2024-07-31 13:58:43 +0300

    Apache Start

---
+++ TMDWalletMaster.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TMDWalletMaster.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
