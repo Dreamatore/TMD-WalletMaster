06b575dd66a1d802a948f2ec3a1ad55171ee2e73
Dreamatore <129581708+Dreamatore@users.noreply.github.com>
2024-08-02 16:24:57 +0300

    +

--- TMD-WalletMaster.Core/obj/Debug/net8.0/TMD-WalletMaster.Core.AssemblyInfo.cs
+++ TMD-WalletMaster.Core/obj/Debug/net8.0/TMD-WalletMaster.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMD-WalletMaster.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea3185dc4e34021c6ea324498448d0c1bb23069")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfdbfe0dce63a45321a07c2907e45043219604b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMD-WalletMaster.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMD-WalletMaster.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
