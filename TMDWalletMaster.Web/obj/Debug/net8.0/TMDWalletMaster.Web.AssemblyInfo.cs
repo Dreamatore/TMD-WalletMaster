98754d26f404fb100d27112316b4599a4a29e74e
Dreamatore <129581708+Dreamatore@users.noreply.github.com>
2024-08-05 14:14:40 +0300

    Apache EF

--- TMDWalletMaster.Web/obj/Debug/net8.0/TMDWalletMaster.Web.AssemblyInfo.cs
+++ TMDWalletMaster.Web/obj/Debug/net8.0/TMDWalletMaster.Web.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMDWalletMaster.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c93e4e9090811036d4011f1ca1585a2fb6250c")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc3bfed5909f4686e5379b91faa2b434c65ab0d")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("TMDWalletMaster.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMDWalletMaster.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
